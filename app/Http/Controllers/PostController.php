9e44579140e4a16ce15427457108167c34cdd9b4
kiskgmskenjinia <tairakeisuke0109tk@gmail.com>
2021-04-08 11:52:17 +0900

    [add]ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
-use App\Http\Requests\PostRequest;
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -17,5 +15,17 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
